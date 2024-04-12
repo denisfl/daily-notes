58ddc0fa4ab1162928170828dcd55a952d93ae98
denisfl <delchyve@gmail.com>
2024-04-12 22:39:25 +0200

    Fix render

--- app/notes/page.js
+++ app/notes/page.js
@@ -1,10 +1,11 @@
 'use client'
 import useRequest from '@/app/hooks/useRequest'
+import query from '@/app/queries/fetchNotes'
 import NotesList from '@/app/components/NotesList'
 import LoadingList from '@/app/components/LoadingList'
 
 export default function Notes() {
-  const { data, isLoading } = useRequest(query, { tag: params.name })
+  const { data, isLoading } = useRequest(query)
   if (isLoading) return <LoadingList />
   return <NotesList notes={data.posts} />
 }
