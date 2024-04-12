6e28c468cacbfdd3ebfcec02fcec1b34d177d7cc
denisfl <delchyve@gmail.com>
2024-04-12 10:20:59 +0200

    Fix meta

--- app/notes/layout.js
+++ app/notes/layout.js
@@ -1,11 +1,9 @@
-import { Inter, Roboto_Slab } from 'next/font/google'
 import TheNavigation from '@/app/components/TheNavigation/index'
 
-const inter = Inter({ subsets: ['latin'] })
-
 export const metadata = {
-  title: 'Notes',
-  description: 'Notes',
+  title: 'Notes by Denis Fedosov-Ledovskikh',
+  description:
+    'Personal page of Denis Fedosov-Ledovskikh. I am frontend developer who loves to make web-services, write notes and learn new things.',
 }
 
 export default function RootLayout({ children }) {
