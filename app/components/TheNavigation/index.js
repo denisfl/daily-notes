60634bc531f264ac3530a25c7f459655901f0d19
denisfl <delchyve@gmail.com>
2024-04-15 10:02:34 +0200

    Add buy me a coffee button

--- app/components/TheNavigation/index.js
+++ app/components/TheNavigation/index.js
@@ -1,7 +1,9 @@
 import { Roboto_Slab } from 'next/font/google'
 import { HomeIcon, NewspaperIcon } from '@heroicons/react/24/outline'
+import Image from 'next/image'
 import styles from './nav.module.css'
 import NavItem from './NavItem'
+import bmcButton from './bmc-button.svg'
 
 const roboto = Roboto_Slab({ subsets: ['cyrillic'] })
 
@@ -9,10 +11,16 @@ export default function TheNavigation() {
   return (
     <nav className={[styles.nav, roboto.className].join(' ')}>
       <NavItem link="/">
-        <HomeIcon className="svg-icon" /> Home
+        <HomeIcon className="svg-icon" />
+        <span>Главная</span>
       </NavItem>
       <NavItem link="/notes">
-        <NewspaperIcon className="svg-icon" /> Notes
+        <NewspaperIcon className="svg-icon" />
+        <span>Заметки</span>
+      </NavItem>
+
+      <NavItem link="https://www.buymeacoffee.com/denisfl" isRight>
+        <Image src={bmcButton} alt="Buy Me A Coffee" height={26} />
       </NavItem>
     </nav>
   )
